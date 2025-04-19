a4014e5669c99a6798f7802d353ea5a3c6b2dd8b
Egor Gordeev <k1zier@mail.ru>
2025-04-20 01:51:55 +0700

    +config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"android/internal/config"
 	"android/internal/delivery/http/handler"
 	"android/internal/repository"
 	"android/internal/server"
 	"android/internal/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	err := config.InitConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	repos := repository.NewRepository()
 	service := service.NewService(repos)
 	handlers := handler.NewHandler(service)
 
 	srv := &server.Server{}
-	err := srv.Run("8080", handlers.InitRoute())
+	err = srv.Run(viper.GetString("8000"), handlers.InitRoute())
 	if err != nil {
 		log.Fatal(err)
 	}
